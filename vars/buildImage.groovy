14a731e9e756f1c0ca53319722c321c062e4c584
diaa ramadan <diaa.ramdan@visionvalley.net>
2023-08-07 14:13:49 +0300

    add parms for docker image build

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t diaa96/shopme:backend-1.0 ./ShopmeWebParent/ShopmeBackend/'
-        sh 'docker build -t diaa96/shopme:frontend-1.0 ./ShopmeWebParent/ShopmeFrontend/'
+        sh "docker build -t $imageName:backend-1.0 ./ShopmeWebParent/ShopmeBackend/"
+        sh "docker build -t $imageName:frontend-1.0 ./ShopmeWebParent/ShopmeFrontend/"
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push diaa96/shopme:backend-1.0'
-        sh 'docker push diaa96/shopme:frontend-1.0'
+        sh "docker push $imageName:backend-1.0"
+        sh "docker push $imageName:frontend-1.0"
     }
 }
