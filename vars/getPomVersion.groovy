1ee620c5467eb3e9c8cc144e3e5255613fb03553
diaa ramadan <diaa.ramdan@visionvalley.net>
2023-09-02 14:52:21 +0300

    remove unused files

--- vars/getPomVersion.groovy
+++
@@ -1,7 +0,0 @@
-#!/user/bin/env groovy
-
-import com.jenkins.Pom
-
-def call(String pomPath) {
-    return new Pom(this).getPomVersion(pomPath)
-}
--- vars/incrementVersion.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-
-import com.jenkins.Pom
-
-def call(String pomPath){
-    return new Pom(this).incrementVersion(pomPath)
-}
